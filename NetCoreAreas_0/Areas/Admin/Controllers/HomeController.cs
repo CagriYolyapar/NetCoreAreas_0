567aea9c158b428db6afd0f365a8fa99052a76d0
cagri.yolyapar <cagri.yolyapar@gmail.com>
2023-11-08 10:08:32 +0300

    Add project files.

---
+++ NetCoreAreas_0/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreAreas_0.Areas.Admin.Controllers
+{
+
+    //Area'lar Attribute olarak tanımlanmadan calıstırılamaz (Yer tutucu operatorlerine .Net 7 itibariyle giremiyor)
+
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
